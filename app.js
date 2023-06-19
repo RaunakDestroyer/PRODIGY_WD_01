a864ead8ddb3bf6918a4feaf7c451fbf293a8f84
RaunakDestroyer <singhraunak1000@gmail.com>
2023-06-19 22:38:03 +0530

    Completed Landing Page

---
+++ app.js
@@ -0,0 +1,35 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const http = require('http');
+
+const hostname = '0.0.0.0';
+const port = 3000;
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+
+
+app.get("/", function(req, res) {
+  res.render("home")
+});
+
+app.get("/skills", function(req, res) {
+  res.render("skills");
+});
+
+app.get("/contacts", function(req, res) {
+  res.render("about");
+});
+
+app.listen(3000, function() {
+  console.log("Server started at port 3000");
+});
